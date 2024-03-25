09896dca5e891b00df6ad8ae3eb005353e7a6388
Amy Lo <amyflo@stanford.edu>
2024-03-25 16:00:10 -0700

    add play page and new portfolio pages

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,49 +1,42 @@
 import * as React from "react"
-import { Link } from "gatsby"
-
-const pageStyles = {
-  color: "#232129",
-  padding: "96px",
-  fontFamily: "-apple-system, Roboto, sans-serif, serif",
-}
-const headingStyles = {
-  marginTop: 0,
-  marginBottom: 64,
-  maxWidth: 320,
-}
-
-const paragraphStyles = {
-  marginBottom: 48,
-}
-const codeStyles = {
-  color: "#8A6534",
-  padding: 4,
-  backgroundColor: "#FFF4DB",
-  fontSize: "1.25rem",
-  borderRadius: 4,
-}
+import "../styles/styles.css"
+import { Heading, Text, Grid, AspectRatio, Divider, Progress, Image, Badge, Box, Flex, NavLink, Card, Container, Button } from "theme-ui"
+import puzzle from "../images/home.png"
+import { Link } from "gatsby";
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer"
 
 const NotFoundPage = () => {
+
+
   return (
-    <main style={pageStyles}>
-      <h1 style={headingStyles}>Page not found</h1>
-      <p style={paragraphStyles}>
-        Sorry 😔, we couldn’t find what you were looking for.
-        <br />
-        {process.env.NODE_ENV === "development" ? (
-          <>
-            <br />
-            Try creating a page in <code style={codeStyles}>src/pages/</code>.
-            <br />
-          </>
-        ) : null}
-        <br />
-        <Link to="/">Go home</Link>.
-      </p>
+    <main>
+      <Navigation />
+      <Grid
+        columns={[1, 1, 1, 2]}
+        gap="0"
+        m="0"
+        bg="#BCED09"
+      >
+        <AspectRatio
+          ratio={1 / 1}
+        >
+          <img width="100%" src={puzzle} alt="puzzle" />
+        </AspectRatio>
+        <Box
+          p={1} bg="blue">
+          <Flex sx={{ flexDirection: 'column', justifyContent: 'flex-end' }} p="4" color="white" >
+            <Heading as="h1" m={[1, 4]}>Oops!.</Heading>
+            <Heading as="h1" m={[1, 4]}>Couldn't find the page you're looking for.</Heading>
+            <Heading as="h1" m={[1, 4]}>While you're here, take a nice long break. Stretch. Get a cup of coffee. Meditate. I'm glad you're here.</Heading>
+            <Link to="/"><Text as="h3" m="4"><u>View my work</u></Text></Link>
+          </Flex>
+        </Box>
+      </Grid>
+      <Footer />
     </main>
   )
 }
-
 export default NotFoundPage
 
-export const Head = () => <title>Not found</title>
+export const Head = () => <title>404: Amy Lo</title>
--- src/pages/index.js
+++ src/pages/index.js
@@ -21,37 +21,45 @@ const IndexPage = () => {
         <AspectRatio
           ratio={1 / 1}
           >
-
           <img width="100%" src={puzzle} alt="puzzle" />
-          </AspectRatio>
+        </AspectRatio>
         <Box
-         p="4" bg="blue">
+         p={1} bg="blue">
             <Flex sx={{ flexDirection: 'column', justifyContent: 'flex-end' }} p="4" color="white" >
-              <Heading as="h1" m="4">Amy Lo is a multidisciplinary designer and developer.</Heading>
-              <Heading as="h1" m="4">She is graduating soon from Stanford with a bachelor's degree in Psychology ('24) and a master's degree in Computer Science, Human-Computer Interaction ('25). Incoming UX Design intern @ Amazon, Summer 2024.</Heading>
-            <Link to="/record"><Text as="h3" m="4"><u>Read my latest case study</u></Text></Link>
+              <Heading as="h1" m={[1, 4]}>Amy Lo is a multidisciplinary designer and developer.</Heading>
+            <Heading as="h1" m={[1, 4]}>She is graduating soon from Stanford with a bachelor's degree in Psychology ('24) and a master's degree in Computer Science, Human-Computer Interaction ('25). Incoming UX Design intern @ Amazon, Summer 2024.</Heading>
+            <Link to="/work"><Text as="h3" m="4"><u>View my work</u></Text></Link>
             </Flex>
         </Box>
-          {/* <Box p="4" bg="blue">
-            <Heading as="h3" m="4" color="white">UI/UX</Heading>
+        <Link to="/projects/record">
+          <Box p="4" bg="#C45BAA">
+            <Heading as="h3" m="4" color="white">UI/UX, Software Development</Heading>
             <Heading as="h1" m="4" color="white">Record</Heading>
-            <Heading as="h2" m="4" color="white">Register now to save 50% and be the first to reserve a seat at the most popular talks.</Heading>
+            <Heading as="h2" m="4" color="white">Music-centric journalling app that analyze your emotions musically. Built with React Native.</Heading>
           </Box>
-        <Box p="4" bg="green">
-          <Heading as="h3" m="4" color="white">UI/UX</Heading>
-          <Heading as="h1" m="4" color="white">Record</Heading>
-          <Heading as="h2" m="4" color="white">Register now to save 50% and be the first to reserve a seat at the most popular talks.</Heading>
+        </Link>
+        <Link to="/projects/paint-ck">
+          <Box p="4" bg="#F9CB40">
+          <Heading as="h3" m="4">Creative Code</Heading>
+          <Heading as="h1" m="4">paint.ck</Heading>
+          <Heading as="h2" m="4">Digital pixel art software and chromatic-scale beat sequencer. Built with ChucK and ChuGL.</Heading>
         </Box>
-        <Box p="4" bg="red">
-          <Heading as="h3" m="4" color="white">UI/UX</Heading>
-          <Heading as="h1" m="4" color="white">Record</Heading>
-          <Heading as="h2" m="4" color="white">Register now to save 50% and be the first to reserve a seat at the most popular talks.</Heading>
-        </Box>
-        <Box p="4" bg="yellow">
-          <Heading as="h3" m="4" color="white">UI/UX</Heading>
-          <Heading as="h1" m="4" color="white">Record</Heading>
-          <Heading as="h2" m="4" color="white">Register now to save 50% and be the first to reserve a seat at the most popular talks.</Heading>
-        </Box> */}
+        </Link>
+        <Link to="/projects/bunnyworld">
+          <Box p="4" bg="#5DA9E9">
+            <Heading as="h3" m="4" >Software Development, Game Design</Heading>
+            <Heading as="h1" m="4" >BunnyWorld</Heading>
+            <Heading as="h2" m="4" >Android game creation tool to make and play games. Built with Java and Android studio.</Heading>
+          </Box>
+        </Link>
+        <Link to="/">
+          <Box p="4" bg="#BCED09">
+            <Heading as="h3" m="4" >Game Design</Heading>
+            <Heading as="h1" m="4" >SpaceBoi vs. the World 🔒</Heading>
+            <Heading as="h2" m="4" >[Coming soon!] Digital game about climate conservation. Built with Unity.</Heading>
+          </Box>
+        </Link>
+       
         </Grid>
       <Footer />
     </main>
